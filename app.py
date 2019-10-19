ae7948dd259cf9a5f3c4e69ddbbb4023675a1d83
Knazran <khairulnazran94@gmail.com>
2019-10-19 13:39:50 +0800

    Added bootstrap

--- app.py
+++ app.py
@@ -13,17 +13,9 @@ application = Flask(__name__)
 def home():
     return render_template('home.html')
 
-@application.route("/course_page",methods=['GET', 'POST'])
-def course_page():
-    return render_template('course.html')
-
-@application.route("/register_course",methods=['GET', 'POST'])
-def register_course():
-    return render_template('register_course.html')
 
 if __name__ == "__main__":
     # Run the API server
-    # app.config['TEMPLATES_AUTO_RELOAD']=True
     print("Starting up Server")
     application.run(debug=True,use_reloader=True)
 	# app.run(debug=True,use_reloader=True)
